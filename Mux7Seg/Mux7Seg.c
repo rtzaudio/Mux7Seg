f81bcd9b2621e9b5d177ea5ce03529b0c376970e
Bob Starr <bobstarr@rtzaudio.com>
2024-01-18 19:35:15 -0500

    blink rate time adjustments

--- Mux7Seg/Mux7Seg.c
+++ Mux7Seg/Mux7Seg.c
@@ -30,9 +30,9 @@
  * settings are for 16Mhz external 18pF crystal.
  */
 FUSES = {
-	.extended = 0xF9,
-	.high     = 0xDF,
-	.low      = 0xF7
+    .extended = 0xF9,
+    .high     = 0xDF,
+    .low      = 0xF7
 };
 
 //LOCKBITS = (0xFF);
@@ -42,32 +42,33 @@ FUSES = {
 /* BCD bit map table for swapped I/O pins */
 #if (HARDWARE_REV < 2)
 static uint8_t g_xlate[] = {
-	0x00,	// 0  = 0000 (BCDA)
-	0x01,	// 1  = 0001
-	0x08,	// 2  = 1000
-	0x09,	// 3  = 1001
-	0x04,	// 4  = 0100
-	0x05,	// 5  = 0101
-	0x0C,	// 6  = 1100
-	0x0D,	// 7  = 1101
-	0x02,	// 8  = 0010
-	0x03,	// 9  = 0011
-	0x0A,	// 10 = 1010
-	0x0B,	// 11 = 1011
-	0x06,	// 12 = 0110
-	0x07,	// 13 = 0111
-	0x0E,	// 14 = 1110
-	0x0F,	// 15 = 1111
+    0x00,	// 0  = 0000 (BCDA)
+    0x01,	// 1  = 0001
+    0x08,	// 2  = 1000
+    0x09,	// 3  = 1001
+    0x04,	// 4  = 0100
+    0x05,	// 5  = 0101
+    0x0C,	// 6  = 1100
+    0x0D,	// 7  = 1101
+    0x02,	// 8  = 0010
+    0x03,	// 9  = 0011
+    0x0A,	// 10 = 1010
+    0x0B,	// 11 = 1011
+    0x06,	// 12 = 0110
+    0x07,	// 13 = 0111
+    0x0E,	// 14 = 1110
+    0x0F,	// 15 = 1111
 };
 #endif
 
 /* BCD 7-seg data buffer */
 static SEGDATA g_segdata;
 
-static volatile uint8_t g_seg0_bcd = 0;
-static volatile uint8_t g_seg1_bcd = 0;
-static volatile uint8_t g_seg2_bcd = 0;
-static volatile uint8_t g_seg3_bcd = 0;
+/* 7-Segment interrupt data buffers */
+static volatile uint8_t g_seg0 = 0;
+static volatile uint8_t g_seg1 = 0;
+static volatile uint8_t g_seg2 = 0;
+static volatile uint8_t g_seg3 = 0;
 
 const char _copyright[] PROGMEM = {
     "STC-1200 7-SEG MUX, Copyright (C) 2024, RTZ Professional Audio"
@@ -100,19 +101,19 @@ void delay_10ms(int count)
 #ifdef DEBUG_MUX
 void debug_tick()
 {
-	if (g_segdata.secs >= 59)
-	{
-		g_segdata.secs = 0;
-	
-		++g_segdata.mins;
-	
-		if (g_segdata.mins >= 59)
-			g_segdata.secs = g_segdata.mins = 0;
-	}
-	else
-	{
-		++g_segdata.secs;
-	}
+    if (g_segdata.secs >= 59)
+    {
+        g_segdata.secs = 0;
+    
+        ++g_segdata.mins;
+    
+        if (g_segdata.mins >= 59)
+            g_segdata.secs = g_segdata.mins = 0;
+    }
+    else
+    {
+        ++g_segdata.secs;
+    }
 }
 #endif
 
@@ -122,12 +123,12 @@ void debug_tick()
 
 int main(void)
 {
-	int c;
-	size_t  i;
-	uint8_t buf[8];
-	uint8_t csum;
-	uint8_t check;	
-	
+    int c;
+    size_t  i;
+    uint8_t buf[8];
+    uint8_t csum;
+    uint8_t check;	
+    
     /* Default time to zero */
     g_segdata.flags = F_PLUS;
     g_segdata.hour  = 0;
@@ -145,152 +146,161 @@ int main(void)
 
     /* Enable global interrupts */
     _SEI();  
-	
-	/* Enter the main loop forever reading display packet data
-	 * from the serial port.
-	 */
-	
+    
+    /* Enter the main loop forever reading display packet data
+     * from the serial port.
+     */
+    
     while(1)
     {
         /* Read a display packet data from the UART. The 
          * display data packet is composed as follows:
-		 *
+         *
          *    Byte   Description
-		 *    ----   -------------------------------------
+         *    ----   -------------------------------------
          *    [0]    Preamble 1, must be 0x89
-		 *    [1]    Preamble 2, must be 0xFC
+         *    [1]    Preamble 2, must be 0xFC
          *    [2]    The SECONDS (0-59)
-		 *    [3]    The MINUTES (0-59)
+         *    [3]    The MINUTES (0-59)
          *    [4]    The HOUR digit (0 or 1)
          *    [5]    The SIGN (negative if zero)
          *    [6]    8-Bit Checksum
-		 */
+         */
 
-		/* Read start of frame preamble 0x89 */
+        /* Read start of frame preamble 0x89 */
 
-		do  {
-			if ((c = usart_tgetc(100)) == -1)
-				goto TimeOut;
+        do  {
+            if ((c = usart_tgetc(100)) == -1)
+                goto TimeOut;
 
-		} while(c != 0x89);
+        } while(c != 0x89);
 
-		/* Read start of frame preamble 0xFC */
-		
-		do  {
-			if ((c = usart_tgetc(100)) == -1)
-				goto TimeOut;
-		} while(c != 0xFC);
+        /* Read start of frame preamble 0xFC */
+        
+        do  {
+            if ((c = usart_tgetc(100)) == -1)
+                goto TimeOut;
+        } while(c != 0xFC);
 
-		/* Read sign, hour, mins, secs and calculate checksum */
-		
-		csum = 0;
-		
-		for (i=0; i < 4; i++)
-		{
-			buf[i] = 0;
-			
-			if ((c = usart_tgetc(100)) == -1)
-				goto TimeOut;
-			
-			/* Buffer the rx packet data */
-			buf[i]= (uint8_t)c;
-			
-			/* Update the checksum */
-			csum += ((uint8_t)c & 0xFF);
-		}
+        /* Read sign, hour, mins, secs and calculate checksum */
+        
+        csum = 0;
+        
+        for (i=0; i < 4; i++)
+        {
+            buf[i] = 0;
+            
+            if ((c = usart_tgetc(100)) == -1)
+                goto TimeOut;
+            
+            /* Buffer the rx packet data */
+            buf[i]= (uint8_t)c;
+            
+            /* Update the checksum */
+            csum += ((uint8_t)c & 0xFF);
+        }
 
-		/* Read the checksum MSB */
-		if ((c = usart_tgetc(100)) == -1)
-			goto TimeOut;
+        /* Read the checksum MSB */
+        if ((c = usart_tgetc(100)) == -1)
+            goto TimeOut;
 
-		check = (uint8_t)c & 0xFF;
+        check = (uint8_t)c & 0xFF;
 
-		/* Validate the CRC values match */
-		
-		if (check == csum)
-		{
-			/* Valid packet received, update display data */
-			g_segdata.secs  = buf[0] & 0x3F;
-			g_segdata.mins  = buf[1] & 0x3F;
-			g_segdata.hour  = buf[2] & 0x01;
-			g_segdata.flags = buf[3];
+        /* Validate the CRC values match */
+        
+        if (check == csum)
+        {
+            /* Valid packet received, update display data */
+            g_segdata.secs  = buf[0] & 0x3F;
+            g_segdata.mins  = buf[1] & 0x3F;
+            g_segdata.hour  = buf[2] & 0x01;
+            g_segdata.flags = buf[3];
 
-            /* Pre calculate the 7-segment BCD values to avoid
-             * math and division in the timer ISR handler.
+            /* Calculate the 7-segment BCD values (division) to
+             * avoid math and division in the timer ISR handler.
              */
-			//_CLI();
-            g_seg0_bcd = g_segdata.secs % 10;
-            g_seg1_bcd = (g_segdata.secs - (g_segdata.secs % 10)) / 10;
-            g_seg2_bcd = g_segdata.mins % 10;
-            g_seg3_bcd = (g_segdata.mins - (g_segdata.mins % 10)) / 10;
-			//_SEI();
-		}
+            
+            //_CLI();
+            g_seg0 = g_segdata.secs % 10;
+            g_seg1 = (g_segdata.secs - (g_segdata.secs % 10)) / 10;
+            g_seg2 = g_segdata.mins % 10;
+            g_seg3 = (g_segdata.mins - (g_segdata.mins % 10)) / 10;
+            //_SEI();
+        }
 
 TimeOut:
-		/* Start the loop over and attempt to synchronize again */
-		csum = 0;
+        /* Start the loop over and attempt to synchronize again */
+        csum = 0;
     }
 }
 
 /****************************************************************************
  * Timer Interrupt Handler for 7-Segment Display Multiplexing
  ***************************************************************************/
+
+/* Given 1 Hz = 1000 ms, the equation for 
+ * hertz to milliseconds is Ms = 1 / Hz ? 1000
+ */
+
+#define BLINK_TIME_MS(ms)   ((uint16_t)((float)ms / (1.0f / (float)MUX_RATE_HZ * 1000.0f)))
                         
 ISR(TIMER0_COMPA_vect)
 {
 #if 0
     /* debug port bit toggle */
-	PORTD = PORTD ^ _BV(PD2);
+    PORTD = PORTD ^ _BV(PD2);
 #else	
     static uint8_t state = 0;
-	static uint16_t blink = 0;
-	
-	/* Handle blanked state logic */
-	if (g_segdata.flags & F_BLANK)
-	{
+    static uint16_t blink = 0;
+    
+    /* Handle blanked state logic */
+    if (g_segdata.flags & F_BLANK)
+    {
         /* Blank hour, plus is preserved */
         PORTD |= _BV(PD_HOUR);
-		/* Turn off all segments */
+        /* Turn off all segments */
         MUX_OFF(PD_TEN_MIN);
         MUX_OFF(PD_UNIT_MIN);
-		MUX_OFF(PD_TEN_SEC);        
-		MUX_OFF(PD_UNIT_SEC);
-		goto exit;
-	}
+        MUX_OFF(PD_TEN_SEC);        
+        MUX_OFF(PD_UNIT_SEC);
+        goto exit;
+    }
 
-	/* Handle blink state logic */
-	
-	if (g_segdata.flags & F_BLINK)
-	{
-		blink++;
-		
-		/* Blink OFF time */
-		if (blink <= (MUX_RATE_HZ/10))
-		{
-			/* Blank hour, plus is preserved */
-			PORTD |= _BV(PD_HOUR);
-			/* Turn off all segments */
-			MUX_OFF(PD_TEN_MIN);
-			MUX_OFF(PD_UNIT_MIN);
-			MUX_OFF(PD_TEN_SEC);
-			MUX_OFF(PD_UNIT_SEC);
-			goto exit;
-		}
+    /* Handle blink state logic */
 
-		/* Blink ON time */
-		if (blink >= (MUX_RATE_HZ/2))
-			blink = 0;
-	}
+    if (g_segdata.flags & F_BLINK)
+    {
+        blink++;
+        
+        /* Blink OFF time */
+        //if (blink <= (MUX_RATE_HZ/10))
+        if (blink <= BLINK_TIME_MS(20))
+        {
+            /* Blank hour, plus is preserved */
+            PORTD |= _BV(PD_HOUR);
+            /* Turn off all segments */
+            MUX_OFF(PD_TEN_MIN);
+            MUX_OFF(PD_UNIT_MIN);
+            MUX_OFF(PD_TEN_SEC);
+            MUX_OFF(PD_UNIT_SEC);
+            goto exit;
+        }
 
-	/*** Set digit/segment value for current multiplex state ***/
-	
+        /* Blink ON time */
+        //if (blink >= (MUX_RATE_HZ/2))
+        if (blink >= BLINK_TIME_MS(100))
+            blink = 0;
+    }
+
+    /*** Set digit/segment value for current multiplex state ***/
+    
     switch(state)
     {
         /*** TENS OF SECONDS ******************/
         case 0:
             /* set tens of sec segment state */
             MUX_OFF(PD_TEN_MIN);            
-			PORTC = BCD_MASK(g_seg0_bcd);
+            PORTC = BCD_MASK(g_seg0);
             MUX_ON(PD_UNIT_SEC);
             ++state;
             break;
@@ -299,7 +309,7 @@ ISR(TIMER0_COMPA_vect)
         case 1:
             /* set unit sec segment state */
             MUX_OFF(PD_UNIT_SEC);
-			PORTC = BCD_MASK(g_seg1_bcd);
+            PORTC = BCD_MASK(g_seg1);
             MUX_ON(PD_TEN_SEC);
             ++state;
             break;
@@ -308,7 +318,7 @@ ISR(TIMER0_COMPA_vect)
         case 2:
             /* set tens of min segment state */
             MUX_OFF(PD_TEN_SEC);
-			PORTC = BCD_MASK(g_seg2_bcd);
+            PORTC = BCD_MASK(g_seg2);
             MUX_ON(PD_UNIT_MIN);
             ++state;
             break;
@@ -317,7 +327,7 @@ ISR(TIMER0_COMPA_vect)
         default:
             /* set unit min segment state */
             MUX_OFF(PD_UNIT_MIN);
-			PORTC = BCD_MASK(g_seg3_bcd);
+            PORTC = BCD_MASK(g_seg3);
             MUX_ON(PD_TEN_MIN);
             state = 0;
             break;
@@ -329,7 +339,7 @@ ISR(TIMER0_COMPA_vect)
      * other segments (as opposed to just on or off).
      */
     
-	if (g_segdata.hour)
+    if (g_segdata.hour)
         PORTD &= ~(_BV(PD_HOUR));
     else
         PORTD |= _BV(PD_HOUR);
@@ -344,7 +354,7 @@ ISR(TIMER0_COMPA_vect)
     
 exit:
 
-	if (g_segdata.flags & F_PLUS)
+    if (g_segdata.flags & F_PLUS)
         PORTD &= ~(_BV(PD_PLUS));
     else
         PORTD |= _BV(PD_PLUS);
@@ -383,8 +393,8 @@ void io_init(void)
 {
     /* PORT-C
      *
-	 * NOTE: PC2 AND PC1 I/O's SWAPPED DUE TO HARDWARE MISTAKE!
-	 *
+     * NOTE: PC2 AND PC1 I/O's SWAPPED DUE TO HARDWARE MISTAKE!
+     *
      * PC_7SEG_A    PC0  (out) gpio out to 7SEG_A
      * PC_7SEG_D    PC1  (out) gpio out to 7SEG_D
      * PC_7SEG_C    PC2  (out) gpio out to 7SEG_C
@@ -395,7 +405,7 @@ void io_init(void)
     PORTC = 0x0;
     /* Set the output pins, all others are inputs */
     DDRC  = _BV(PC_7SEG_A) | _BV(PC_7SEG_B) | _BV(PC_7SEG_C) | _BV(PC_7SEG_D);
-	_NOP();
+    _NOP();
 
     /* PORT-D
      *
@@ -414,8 +424,8 @@ void io_init(void)
     /* Set the output pins, all others are inputs */
     DDRD  = _BV(PD_TXD) | _BV(PD_TEN_SEC) | _BV(PD_UNIT_SEC) | _BV(PD_TEN_MIN) |
             _BV(PD_UNIT_MIN) | _BV(PD_HOUR) | _BV(PD_PLUS);
-	_NOP();
-	
+    _NOP();
+    
     /* All high to disable */
     PORTD |= _BV(PD_TEN_SEC) | _BV(PD_UNIT_SEC) | _BV(PD_TEN_MIN) |
              _BV(PD_UNIT_MIN) | _BV(PD_HOUR) | _BV(PD_PLUS);
@@ -431,11 +441,11 @@ void io_init(void)
 #ifdef SPI_INTERFACE
     /* all pins low, no pullups */
     PORTB = 0x00;
-	/* All pins input for now */
-	DDRB  = 0;
-	/* Enable pullups on SSEL, MOSI, SCK */
+    /* All pins input for now */
+    DDRB  = 0;
+    /* Enable pullups on SSEL, MOSI, SCK */
     //PORTB = _BV(PB_SSEL) | _BV(PB_MOSI) | _BV(PB_SCK);
-	_NOP();	
+    _NOP();	
     /* Set MISO to output, all others are inputs */
     DDRB  = _BV(PB_MISO);
 #endif
@@ -449,108 +459,108 @@ void io_init(void)
 
 void spi_init_slave(void)
 {
-	uint8_t d;
+    uint8_t d;
     
     /* Configure SPI and interrupt enable, 
-	 *
-	 * SPR0 = 0 : clock rate select - no effect for slave mode
-	 * SPR1 = 0 : clock rate select - no effect for slave mode
-	 * CPHA = 0 : clock phase
-	 * CPOL = 0 : clock polarity (SCK high when idle)
-	 * MSTR = 0 : clear for slave mode
-	 * DORD = 0 : MSB transmitted first
-	 * SPE  = 1 : SPI enable
-	 * SPIE = 1 : interrupt enable
-	 */    
+     *
+     * SPR0 = 0 : clock rate select - no effect for slave mode
+     * SPR1 = 0 : clock rate select - no effect for slave mode
+     * CPHA = 0 : clock phase
+     * CPOL = 0 : clock polarity (SCK high when idle)
+     * MSTR = 0 : clear for slave mode
+     * DORD = 0 : MSB transmitted first
+     * SPE  = 1 : SPI enable
+     * SPIE = 1 : interrupt enable
+     */    
     SPCR = _BV(SPE) | _BV(SPIE);
     
-	/* Clear interrupt by reading SPSR & SPDR registers */
-	d = SPSR;
-	d = SPDR;
-	d = 0;
-	SPDR = d;
+    /* Clear interrupt by reading SPSR & SPDR registers */
+    d = SPSR;
+    d = SPDR;
+    d = 0;
+    SPDR = d;
 }
 
 /* Function to send and receive a data byte */
 unsigned char spi_transaction(unsigned char data)
 {
-	SPDR = data;                    /* Load data into the buffer */
-	while((SPSR & (1<<SPIF) ));		/* Wait until transmission complete */
-	return SPDR;                    /* Return received data */
+    SPDR = data;                    /* Load data into the buffer */
+    while((SPSR & (1<<SPIF) ));		/* Wait until transmission complete */
+    return SPDR;                    /* Return received data */
 }
 
 /* SPI slave receive data interrupt handler */
 ISR(SPI_STC_vect)
 {
-	uint16_t d;
-	static uint16_t data = 0;
-	static uint8_t state = 0;
-	
-	/* Read the data register */
-	d = (uint16_t)SPDR;
-			
-	/* Read the data register */
-	if (!state)
-	{
-		/* First byte, the MSB, read completed */
-		state = 1;
-    	data = (d << 8);
-	}
-	else
-	{
-		/* Second byte, the LSB, read completed */
-		state = 0;
-		data |= d;
-		
-	    /* Read two bytes from the UART for a 16-bit word. This
-	     * word specifies the sign, hour, minutes and seconds to
-	     * display on the 7-segment H:MM:SS display with sign. 
-	     *
-	     *    S (bits 1-6)     Specifies the SECONDS (0-59).
-	     *    M (bits 7-12)    Specifies the MINUTES (0-59).
-	     *    H (bit 13)       Specifies the HOUR digit (0 or 1).
-	     *    B (bit 14)       Sets BLANKING mode, all segments off.
-	     *    F (bit 15)       Sets FLASHING mode all segments.
-	     *    N (bit 16)       Specifies the SIGN as negative.
-	     * 
-	     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	     *    |N|B|F|H|M|M|M|M|M|M|S|S|S|S|S|S|
-	     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	     */
-	
-		/* set seconds segment value */
-		g_segdata.secs = data & 0x3F;
-				
-		/* set minutes segment value */
-		g_segdata.mins = (data >> 6) & 0x3F;
-				
-		/* set hour segment value from bit-13 */
-		g_segdata.hour = (data & 0x1000) ? 1: 0;
-	
-		/* set the blanking bit-14 flag mask */
-		if (data & 0x2000)
-			g_segdata.flags |= F_BLANK;
-		else
-			g_segdata.flags &= ~(F_BLANK);
-			
-		/* set the blink bit-15 flag mask */
-		if (data & 0x4000)
-			g_segdata.flags |= F_BLINK;
-		else
-			g_segdata.flags &= ~(F_BLINK);
+    uint16_t d;
+    static uint16_t data = 0;
+    static uint8_t state = 0;
+    
+    /* Read the data register */
+    d = (uint16_t)SPDR;
+            
+    /* Read the data register */
+    if (!state)
+    {
+        /* First byte, the MSB, read completed */
+        state = 1;
+        data = (d << 8);
+    }
+    else
+    {
+        /* Second byte, the LSB, read completed */
+        state = 0;
+        data |= d;
+        
+        /* Read two bytes from the UART for a 16-bit word. This
+         * word specifies the sign, hour, minutes and seconds to
+         * display on the 7-segment H:MM:SS display with sign. 
+         *
+         *    S (bits 1-6)     Specifies the SECONDS (0-59).
+         *    M (bits 7-12)    Specifies the MINUTES (0-59).
+         *    H (bit 13)       Specifies the HOUR digit (0 or 1).
+         *    B (bit 14)       Sets BLANKING mode, all segments off.
+         *    F (bit 15)       Sets FLASHING mode all segments.
+         *    N (bit 16)       Specifies the SIGN as negative.
+         * 
+         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         *    |N|B|F|H|M|M|M|M|M|M|S|S|S|S|S|S|
+         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         */
+    
+        /* set seconds segment value */
+        g_segdata.secs = data & 0x3F;
+                
+        /* set minutes segment value */
+        g_segdata.mins = (data >> 6) & 0x3F;
+                
+        /* set hour segment value from bit-13 */
+        g_segdata.hour = (data & 0x1000) ? 1: 0;
+    
+        /* set the blanking bit-14 flag mask */
+        if (data & 0x2000)
+            g_segdata.flags |= F_BLANK;
+        else
+            g_segdata.flags &= ~(F_BLANK);
+            
+        /* set the blink bit-15 flag mask */
+        if (data & 0x4000)
+            g_segdata.flags |= F_BLINK;
+        else
+            g_segdata.flags &= ~(F_BLINK);
 
-		/* set plus segment bit-16 flag mask */
-		if (data & 0x8000)		
-			g_segdata.flags |= F_PLUS;
-		else
-			g_segdata.flags &= ~(F_PLUS);
+        /* set plus segment bit-16 flag mask */
+        if (data & 0x8000)		
+            g_segdata.flags |= F_PLUS;
+        else
+            g_segdata.flags &= ~(F_PLUS);
 
         /* Update display mux 7-seg data */
-        g_seg0_bcd = g_segdata.secs % 10;
-        g_seg1_bcd = (g_segdata.secs - (g_segdata.secs % 10)) / 10;
-        g_seg2_bcd = g_segdata.mins % 10;
-        g_seg3_bcd = (g_segdata.mins - (g_segdata.mins % 10)) / 10;
-	}
+        g_seg0 = g_segdata.secs % 10;
+        g_seg1 = (g_segdata.secs - (g_segdata.secs % 10)) / 10;
+        g_seg2 = g_segdata.mins % 10;
+        g_seg3 = (g_segdata.mins - (g_segdata.mins % 10)) / 10;
+    }
 }
 
 #endif
